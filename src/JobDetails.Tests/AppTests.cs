2d8e11b10bfe598c4069f5d8f94351077c36f831
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-07 15:10:29 +1200

    abstracting App functionality

--- src/JobDetails.Tests/AppTests.cs
+++ src/JobDetails.Tests/AppTests.cs
@@ -11,7 +11,8 @@ using System.IO;
 
 namespace JobDetails.Tests
 {
-    public class Job {
+    public class Job
+    {
         public readonly string Title;
         public readonly string Company;
         public readonly string Description;
@@ -24,67 +25,75 @@ namespace JobDetails.Tests
         }
     }
 
-    public class JobConfig {
+    public class JobConfig
+    {
         public readonly string ScriptTemplate;
         public readonly string Path;
         public readonly string[] Title;
         public readonly string[] Company;
         public readonly string[] Description;
 
-        public JobConfig(JsonReader.Reader reader) {
+        public JobConfig(JsonReader.Reader reader)
+        {
             this.ScriptTemplate = reader.GetString(new string[] { "job", "scriptTemplate" });
-            this.Path = reader.GetString(new string[] {"job", "path"});
-            this.Title = reader.GetItems<string>(new string[] {"job", "title"}, new MapStringValue());
-            this.Company = reader.GetItems<string>(new string[] {"job", "company"}, new MapStringValue());
-            this.Description = reader.GetItems<string>(new string[] {"job", "description"}, new MapStringValue());
+            this.Path = reader.GetString(new string[] { "job", "path" });
+            this.Title = reader.GetItems<string>(new string[] { "job", "title" }, new MapStringValue());
+            this.Company = reader.GetItems<string>(new string[] { "job", "company" }, new MapStringValue());
+            this.Description = reader.GetItems<string>(new string[] { "job", "description" }, new MapStringValue());
         }
     }
 
-    public class JobDetailsConfig {
+    public class JobDetailsConfig
+    {
         public readonly string Source;
         public readonly JobConfig Job;
 
         public JobDetailsConfig(string json)
         {
             var jsonReader = new JsonReader.Reader(json);
-            this.Source = jsonReader.GetString(new string[] {"source"});
+            this.Source = jsonReader.GetString(new string[] { "source" });
             this.Job = new JobConfig(jsonReader);
         }
 
     }
 
-        public class App {
-            private readonly JobDetailsConfig config;
-            public App(JobDetailsConfig config) {
-                this.config = config;
-            }
-
-            public async ValueTask<Job> GetJob() {
-                var httpClient = new HttpClient();
-                var results = await httpClient.GetAsync(config.Source);
-
-                Assert.AreEqual(HttpStatusCode.OK, results.StatusCode);
-
-                var html = await results.Content.ReadAsStringAsync();
-                var htmlReader = new HtmlReader.Reader(html);
-                var scriptTemplate = config.Job.ScriptTemplate;
-                var script = htmlReader.ParseScript(config.Job.Path, scriptTemplate);
-
-                var jsonReader = new JsonReader.Reader(script);
-                return new Job(jsonReader, config);
-            }
+    public class App
+    {
+        private readonly JobDetailsConfig config;
+        public App(JobDetailsConfig config)
+        {
+            this.config = config;
         }
 
-    public class TestConfig {
+        public async ValueTask<Job> GetJob()
+        {
+            var httpClient = new HttpClient();
+            var results = await httpClient.GetAsync(config.Source);
+
+            Assert.AreEqual(HttpStatusCode.OK, results.StatusCode);
+
+            var html = await results.Content.ReadAsStringAsync();
+            var htmlReader = new HtmlReader.Reader(html);
+            var scriptTemplate = config.Job.ScriptTemplate;
+            var script = htmlReader.ParseScript(config.Job.Path, scriptTemplate);
+
+            var jsonReader = new JsonReader.Reader(script);
+            return new Job(jsonReader, config);
+        }
+    }
+
+    public class TestConfig
+    {
         public readonly string Title;
         public readonly string Company;
         public readonly string Description;
 
-        public TestConfig(string json) {
+        public TestConfig(string json)
+        {
             var jsonReader = new JsonReader.Reader(json);
-            Title =  jsonReader.GetString(new string[] {"title"});
-            Company =  jsonReader.GetString(new string[] {"company"});
-            Description =  jsonReader.GetString(new string[] {"description"});
+            Title = jsonReader.GetString(new string[] { "title" });
+            Company = jsonReader.GetString(new string[] { "company" });
+            Description = jsonReader.GetString(new string[] { "description" });
         }
     }
 
@@ -92,14 +101,16 @@ namespace JobDetails.Tests
     {
         private JobDetailsConfig config;
         private TestConfig testConfig;
-        
+
         [SetUp]
         public void Setup()
         {
-            using(var reader = new StreamReader("../../../app-config.json")) {
+            using (var reader = new StreamReader("../../../app-config.json"))
+            {
                 config = new JobDetailsConfig(reader.ReadToEnd());
             }
-            using (var reader = new StreamReader("../../../test-config.json")) {
+            using (var reader = new StreamReader("../../../test-config.json"))
+            {
                 testConfig = new TestConfig(reader.ReadToEnd());
             }
         }
