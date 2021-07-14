7887093878ab52eaa6e5b7318f96c4bb7f7c7774
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-14 13:47:25 +1200

    jobdetails console config reads arguments from parameters

--- src/JobDetails.Console/Program.cs
+++ src/JobDetails.Console/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using JobDetails;
+using System.IO;
 
 namespace JobDetails.Console
 {
+    public class Config {
+        public readonly string JobDetailsPath;
+        public readonly string Source;
+
+        public Config(string[] args)
+        {
+            this.JobDetailsPath = args[0];
+            this.Source = args[1];
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new Config(args);
+
+            System.Console.WriteLine(config.JobDetailsPath);
+            System.Console.WriteLine(config.Source);
         }
     }
 }
