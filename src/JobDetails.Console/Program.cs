4d23af5cefbcfe0f25a0bfcd90213ebce32b3d24
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-15 07:36:03 +1200

    adding list items functionality

--- src/JobDetails.Console/Program.cs
+++ src/JobDetails.Console/Program.cs
@@ -64,16 +64,24 @@ namespace JobDetails.Console
         static async Task Main(string[] args)
         {
             var config = new Config(args);
-            System.Console.WriteLine(config.JobDetailsPath);
-            System.Console.WriteLine(config.Source);            
+            if (config.Action == "list") {
+                var data = JsonConvert.DeserializeObject<IDictionary<string, string>>(File.ReadAllText(config.JobDetailsPath));
+                foreach (var key in data.Keys)
+                {
+                    System.Console.WriteLine(key);
+                }
+            } else {
+                System.Console.WriteLine(config.JobDetailsPath);
+                System.Console.WriteLine(config.Source);            
 
-            var appConfig = File.ReadAllText(config.AppConfigPath);
-            System.Console.WriteLine(config.Action);
-            var app = new App(new JobDetailsConfig(appConfig.Replace("{source}", config.Source)), new DataStore(config.JobDetailsPath), new HttpClient());
+                var appConfig = File.ReadAllText(config.AppConfigPath);
+                System.Console.WriteLine(config.Action);
+                var app = new App(new JobDetailsConfig(appConfig.Replace("{source}", config.Source)), new DataStore(config.JobDetailsPath), new HttpClient());
 
-            var job = await app.GetJob();
-            System.Console.WriteLine($"job: title: {job.Title}");
-            System.Console.WriteLine($"job: company: {job.Company}");
+                var job = await app.GetJob();
+                System.Console.WriteLine($"job: title: {job.Title}");
+                System.Console.WriteLine($"job: company: {job.Company}");
+            }
         }
     }
 }
